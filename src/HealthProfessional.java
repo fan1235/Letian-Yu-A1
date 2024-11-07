89bb181ef6cf5262ff3dca60c3e9607ec40b4b4f
fan1296 <165129265+fan1235@users.noreply.github.com>
2024-11-07 20:56:28 +0800

    Part 1 Code Writing

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,2 +1,25 @@
 public class HealthProfessional {
+        private int ID;
+        private String name;
+        private String specialization;
+
+        public HealthProfessional() {
+            this.ID = 0;
+            this.name = "Unknown";
+            this.specialization = "General";
+        }
+
+        public HealthProfessional(int ID, String name, String specialization) {
+            this.ID = ID;
+            this.name = name;
+            this.specialization = specialization;
+        }
+
+        public void displayInfo() {
+            System.out.println("ID: " + ID);
+            System.out.println("Name: " + name);
+            System.out.println("Specialization: " + specialization);
+        }
+
+
 }
