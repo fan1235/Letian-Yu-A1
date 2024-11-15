0aa473b651cb38f23def90f56045784110b2c250
fan1296 <165129265+fan1235@users.noreply.github.com>
2024-11-15 20:57:08 +0800

    Completed Part 5 of the assignment, which involves managing appointments using an ArrayList and providing methods for creating, displaying, and canceling appointments.

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -33,8 +33,65 @@ public class AssignmentOne {
 
         // Part 5 – Collection of appointments
         ArrayList<Appointment> appointments = new ArrayList<>();
-        
 
+
+        createAppointment(appointments, new Patient("Shuang Zhang", "13001258707", 30, "2893464201@qq.com"), "10:25", generalPractitioner1);
+        createAppointment(appointments, new Patient("Tao Zhou", "13716376774", 28, "2464584423@qq.com"), "14:17", surgeon1);
+        createAppointment(appointments, new Patient("Le Chang", "13701268707", 29, "2896434321@qq.com"), "09:06", generalPractitioner2);
+        createAppointment(appointments, new Patient("Jing Xv", "13910151196", 42, "2456584430@qq.com"), "15:27", surgeon2);
+
+
+        printExistingAppointments(appointments);
+
+        System.out.println("------------------------------");
+
+
+        cancelBooking(appointments, "19101279884");
+        System.out.println("------------------------------");
+
+
+        printExistingAppointments(appointments);
+        System.out.println("------------------------------");
+    }
+
+
+    public static void createAppointment(ArrayList<Appointment> appointments, Patient patient, String timeSlot, HealthProfessional doctor) {
+        if (patient != null && timeSlot != null && !timeSlot.isEmpty() && doctor != null) {
+            Appointment appointment = new Appointment(patient, timeSlot, doctor);
+            appointments.add(appointment);
+            System.out.println("Appointment created for " + patient.getName() + " with " + doctor.getName() + " at " + timeSlot);
+        } else {
+            System.out.println("Sorry, you are missing information and unable to create an appointment.");
+        }
+    }
+
+
+    public static void printExistingAppointments(ArrayList<Appointment> appointments) {
+        if (appointments.isEmpty()) {
+            System.out.println("Sorry,No existing appointments.");
+        } else {
+            System.out.println("Existing appointments:");
+            for (Appointment appointment : appointments) {
+                appointment.displayAppointmentDetails();
+                System.out.println("------------------------------");
+            }
+        }
+    }
+
+
+    public static void cancelBooking(ArrayList<Appointment> appointments, String mobilePhone) {
+        boolean found = false;
+        for (int i = 0; i < appointments.size(); i++) {
+            if (appointments.get(i).getPatient().getMobilePhone().equals(mobilePhone)) {
+                appointments.remove(i);
+                found = true;
+                System.out.println("Appointment with patient " + mobilePhone + " has been cancelled.");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Sorry, we couldn't find any appointments for patients using mobile phones" + mobilePhone);
+        }
     }
 
 
